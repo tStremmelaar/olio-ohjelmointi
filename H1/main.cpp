1b828557abfd5de670eb37ac9101cb2ff21a27ac
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-02 19:28:09 +0200

    get inputs

--- H1/main.cpp
+++ H1/main.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
+    int a, b;
+
+    // get values for integers
+    cout << "Enter an integer: ";
+    cin >> a;
+    cout << "Enter another integer: ";
+    cin >> b;
+
+    //cout << a << endl << b << endl; // debug print
+
     return 0;
 }
