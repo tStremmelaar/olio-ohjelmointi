fb9100e4b4ff49587e4d49cfa0b871350980e0ed
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-09 14:37:25 +0200

    created members and methods of Car

--- H2b/car.cpp
+++ H2b/car.cpp
@@ -1,3 +1,15 @@
 #include "car.h"
 
-Car::Car() {}
+Car::Car(string newBrand, string newModel, int newYearModel)
+{
+    brand = newBrand;
+    model = newModel;
+    yearModel = newYearModel;
+}
+
+void Car::printData()
+{
+    cout << "Brand:\t" << brand << endl;
+    cout << "Model:\t" << model << endl;
+    cout << "Year:\t" << yearModel << endl << endl;
+}
