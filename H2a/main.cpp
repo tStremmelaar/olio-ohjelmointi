3db7bc962bb77bb93acb369ee70b26761138e218
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-09 13:34:25 +0200

    created object of Car

--- H2a/main.cpp
+++ H2a/main.cpp
@@ -1,4 +1,12 @@
+#include "car.h"
+
 int main()
 {
+    Car objCar;
+    objCar.setBrand("Toyota");
+    objCar.setModel("Corolla");
+    objCar.setYearModel(1998);
+    objCar.printData();
+
     return 0;
 }
