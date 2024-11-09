a63a36a6ec2f6136a5051bece082f623dc47e847
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-09 14:58:02 +0200

    created vector of class Car

--- H2b/main.cpp
+++ H2b/main.cpp
@@ -1,6 +1,25 @@
-using namespace std;
+#include "car.h"
+
+#include <vector>
 
 int main()
 {
+    vector<Car> carList;
+
+    Car firstCar("Citroen", "BX", 1990);
+    Car secondCar("Toyota", "Corolla", 1998);
+    Car thirdCar("Honda", "Civic", 2001);
+
+    carList.push_back(firstCar);
+    carList.push_back(secondCar);
+    carList.push_back(thirdCar);
+
+    carList[1].printData();
+
+    for(int i = 0; i < 3; ++i)
+    {
+        carList[i].printData();
+    }
+
     return 0;
 }
