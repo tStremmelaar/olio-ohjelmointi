ecfe95706c64e6525139e39f239e3123624e17df
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-09 14:02:46 +0200

    created object of Rectangle

--- H2a/main.cpp
+++ H2a/main.cpp
@@ -1,4 +1,5 @@
 #include "car.h"
+#include "rectangle.h"
 
 int main()
 {
@@ -7,6 +8,14 @@ int main()
     objCar.setModel("Corolla");
     objCar.setYearModel(1998);
     objCar.printData();
+
+    Rectangle * objRectangle = new Rectangle;
+    objRectangle->setWidth(32.4);
+    objRectangle->setHeight(24.4);
+    cout << "Area:\t\t" << objRectangle->getArea() << endl;
+    cout << "Circumference:\t" << objRectangle->getCircum() << endl << endl;
+    delete objRectangle;
+    objRectangle = nullptr;
 
     return 0;
 }
