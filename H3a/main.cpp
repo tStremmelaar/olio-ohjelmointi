68bef7c86f779e6edc0a1865f7cc7c6f307b0282
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-12 20:30:28 +0200

    added object of class ItalianChef

--- H3a/main.cpp
+++ H3a/main.cpp
@@ -1,9 +1,18 @@
 #include "chef.h"
+#include "italianchef.h"
 
 int main()
 {
     Chef objChef("Gordon Ramsay");
     objChef.makeSalad();
     objChef.makeSoup();
+
+    ItalianChef objItalianChef("Anthony Bourdain");
+    objItalianChef.makeSalad();
+    objItalianChef.makeSoup();
+    objItalianChef.makePasta();
+    cout << "name of the Italian Chef is "
+         << objItalianChef.getName() << endl;
+
     return 0;
 }
