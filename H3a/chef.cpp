8530ddb6005600068c0d02291f51583427834cc6
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-12 16:11:37 +0200

    added members and methods to Chef

--- H3a/chef.cpp
+++ H3a/chef.cpp
@@ -1,3 +1,22 @@
 #include "chef.h"
 
-Chef::Chef() {}
+Chef::Chef(string newName)
+{
+    name = "Chef " + newName + " ";
+    cout << name << "konstruktori" << endl;
+}
+
+void Chef::makeSalad()
+{
+    cout << name << "makes salad" << endl;
+}
+
+void Chef::makeSoup()
+{
+    cout << name << "makes soup" << endl;
+}
+
+Chef::~Chef()
+{
+    cout << name << "destruktori" << endl;
+}
--- H3a/chef.h
+++ H3a/chef.h
@@ -1,10 +1,18 @@
 #ifndef CHEF_H
 #define CHEF_H
 
+#include <iostream>
+using namespace std;
+
 class Chef
 {
+protected:
+    string name;
 public:
-    Chef();
+    Chef(string);
+    void makeSalad();
+    void makeSoup();
+    ~Chef();
 };
 
 #endif // CHEF_H
