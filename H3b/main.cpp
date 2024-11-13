828009e8dbb3a904b862cab2417a5bfa402df75d
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-13 15:02:32 +0200

    created pointer to class Animal

--- H3b/main.cpp
+++ H3b/main.cpp
@@ -1,4 +1,15 @@
+#include "animal.h"
+#include "dog.h"
+
+#include <windows.h>
+
 int main()
 {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    Animal *ptrAnimal = new Animal();
+    ptrAnimal->callOut();
+    delete ptrAnimal;
+    ptrAnimal = nullptr;
     return 0;
 }
