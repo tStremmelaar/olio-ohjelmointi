002951febdf1043c4699d45824f5814ba71a9bf0
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-13 15:20:14 +0200

    created pointer to class Dog

--- H3b/main.cpp
+++ H3b/main.cpp
@@ -1,15 +1,16 @@
 #include "animal.h"
 #include "dog.h"
 
-#include <windows.h>
-
 int main()
 {
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
     Animal *ptrAnimal = new Animal();
     ptrAnimal->callOut();
     delete ptrAnimal;
     ptrAnimal = nullptr;
+
+    Dog *ptrDog = new Dog();
+    ptrDog->callOut();
+    delete ptrDog;
+    ptrDog = nullptr;
     return 0;
 }
