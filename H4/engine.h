896929aff213796e605f222408bb4252879269d9
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-19 16:35:26 +0200

    created Engine header

--- H4/engine.h
+++ H4/engine.h
@@ -3,8 +3,16 @@
 
 class Engine
 {
+private:
+    int horsepower;
+    double displacement;
 public:
     Engine();
+    Engine(int, double);
+    int getHorsepower() const;
+    void setHorsepower(int newHorsepower);
+    double getDisplacement() const;
+    void setDisplacement(double newDisplacement);
 };
 
 #endif // ENGINE_H
