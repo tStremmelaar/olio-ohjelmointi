ebfa94c93a4eb3bca70ac09d4ffd26a7faa87b86
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-11-19 16:18:16 +0200

    created class Car

---
+++ H4/car.cpp
@@ -0,0 +1,63 @@
+#include "car.h"
+
+void Car::setEngine()
+{
+    objEngine.setHorsepower(150);
+    objEngine.setDisplacement(2.0);
+}
+
+void Car::setWheels()
+{
+    objWheel1.setSize(17);
+    objWheel2.setSize(17);
+    objWheel3.setSize(17);
+    objWheel4.setSize(17);
+
+    objWheel1.setType("kesarengas");
+    objWheel2.setType("kesarengas");
+    objWheel3.setType("kesarengas");
+    objWheel4.setType("kesarengas");
+}
+
+string Car::getModel() const
+{
+    return model;
+}
+
+void Car::setModel(const string &newModel)
+{
+    model = newModel;
+}
+
+string Car::getBrand() const
+{
+    return brand;
+}
+
+void Car::setBrand(const string &newBrand)
+{
+    brand = newBrand;
+}
+
+void Car::printDetails()
+{
+    cout << "Auto: " << getBrand() << " " << getModel() << endl;
+    cout << "Moottori: " << objEngine.getHorsepower() << " hp "
+         << objEngine.getDisplacement() << " L" << endl;
+    cout << "Rengas 1: " << objWheel1.getSize() << " tuumaa, "
+         << objWheel1.getType() << endl;
+    cout << "Rengas 2: " << objWheel2.getSize() << " tuumaa, "
+         << objWheel2.getType() << endl;
+    cout << "Rengas 3: " << objWheel3.getSize() << " tuumaa, "
+         << objWheel3.getType() << endl;
+    cout << "Rengas 4: " << objWheel4.getSize() << " tuumaa, "
+         << objWheel4.getType() << endl;
+}
+
+Car::Car() {}
+
+Car::Car(string newModel, string newBrand)
+{
+    model = newModel;
+    brand = newBrand;
+}
