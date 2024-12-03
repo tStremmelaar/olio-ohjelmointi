064da6754426c4eb1fd72bff898866d555996fec
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-12-03 13:55:00 +0200

    aggregation done

--- H5/main.cpp
+++ H5/main.cpp
@@ -1,5 +1,6 @@
 #include <classb.h>
 #include <associationa.h>
+#include <aggregationa.h>
 
 #include <iostream>
 
@@ -13,24 +14,31 @@ int main()
          << &a << endl;
     cout << "b:n arvo on: " << b << " ja osoite on: "
          << &b << endl;
+    cout << endl;
 
     int* myPointer = &a;
-    cout << "\nPointterin osoittama osoite on: " << myPointer << endl;
+    cout << "Pointterin osoittama osoite on: " << myPointer << endl;
     cout << "Pointterin osoittaman muistipaikan arvo on: "
          << *myPointer << endl;
+    cout << endl;
 
     myPointer = &b;
-    cout << "\nPointterin osoittama osoite on: " << myPointer << endl;
+    cout << "Pointterin osoittama osoite on: " << myPointer << endl;
     cout << "Pointterin osoittaman muistipaikan arvo on: "
          << *myPointer << endl;
+    cout << endl;
 
     int& refA = a;
-    cout << "\nrefA osoittaa osoitteeseen: " << &refA << endl;
+    cout << "refA osoittaa osoitteeseen: " << &refA << endl;
     cout << "refA:n osoittaman muistipaikan arvo on: "
-         << refA << "\n" << endl;
+         << refA << endl;
+    cout << endl;
 
     refA = b;
     // refA:ta ei saa osoittamaan b:n osoitteeseen
+
+
+    cout << "*************************************" << endl;
 
 
     ClassB objB;
@@ -42,6 +50,16 @@ int main()
     cout << "Assosiaatio esimerkki:" << endl;
     cout << "objB: " << objB.getInfo() << endl;
     cout << "objAss: " << objAss.getBinfo() << endl;
+    cout << endl;
+
+
+    cout << "Aggregaatio esimerkki:" << endl;
+    ClassB &refB = objB;
+    AggregationA objAggr(refB);
+    objAggr.setBinfo("Olion Agr asettama info");
+    cout << "objB: " << objB.getInfo() << endl;
+    cout << "objAggr: " << objAggr.getBinfo() << endl;
+    cout << endl;
 
     return 0;
 }
