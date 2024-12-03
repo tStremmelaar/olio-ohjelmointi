f60052a5665d715327fcd37c3aa9900a77e0f061
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-12-03 13:12:16 +0200

    pointers and references finished

--- H5/main.cpp
+++ H5/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+    int a = 5;
+    int b = 10;
+    cout << "a:n arvo on: " << a << " ja osoite on: "
+         << &a << endl;
+    cout << "b:n arvo on: " << b << " ja osoite on: "
+         << &b << endl;
+
+    int* myPointer = &a;
+    cout << "\nPointterin osoittama osoite on: " << myPointer << endl;
+    cout << "Pointterin osoittaman muistipaikan arvo on: "
+         << *myPointer << endl;
+
+    myPointer = &b;
+    cout << "\nPointterin osoittama osoite on: " << myPointer << endl;
+    cout << "Pointterin osoittaman muistipaikan arvo on: "
+         << *myPointer << endl;
+
+    int& refA = a;
+    cout << "\nrefA osoittaa osoitteeseen: " << &refA << endl;
+    cout << "refA:n osoittaman muistipaikan arvo on: "
+         << refA << "\n" << endl;
+
+    refA = b;
+    // refA:ta ei saa osoittamaan b:n osoitteeseen
     return 0;
 }
