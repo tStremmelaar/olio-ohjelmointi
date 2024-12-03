66591e52c6e770b8ef7405d149be057c822544e7
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-12-03 13:44:48 +0200

    added member and methods for AggregationA

--- H5/aggregationa.h
+++ H5/aggregationa.h
@@ -1,10 +1,16 @@
 #ifndef AGGREGATIONA_H
 #define AGGREGATIONA_H
 
+#include "classb.h"
+
 class AggregationA
 {
+private:
+    ClassB &refB;
 public:
-    AggregationA();
+    AggregationA(ClassB &);
+    string getBinfo();
+    void setBinfo(string);
 };
 
 #endif // AGGREGATIONA_H
