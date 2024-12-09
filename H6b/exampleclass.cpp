7d1639d508837d5da36e8edf278f3a72c463994d
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-12-09 15:01:17 +0200

    created members and methods

--- H6b/exampleclass.cpp
+++ H6b/exampleclass.cpp
@@ -2,4 +2,22 @@
 
 ExampleClass::ExampleClass(QObject *parent)
     : QObject{parent}
-{}
+{
+    qDebug() << "Start";
+
+    // connect...
+    QObject::connect(parent, SIGNAL(readyToSay()), this, SLOT(sayHelloSlot()));
+
+    qDebug() << "End";
+}
+
+void ExampleClass::startToWait()
+{
+    QThread::msleep(1000);
+    emit readyToSay();
+}
+
+void ExampleClass::sayHelloSlot()
+{
+    qDebug() << "Terve";
+}
--- H6b/exampleclass.h
+++ H6b/exampleclass.h
@@ -2,14 +2,19 @@
 #define EXAMPLECLASS_H
 
 #include <QObject>
+#include <QThread>
+#include <QCoreApplication>
 
 class ExampleClass : public QObject
 {
     Q_OBJECT
 public:
     explicit ExampleClass(QObject *parent = nullptr);
-
+    void startToWait();
 signals:
+    void readyToSay();
+public slots:
+    void sayHelloSlot();
 };
 
 #endif // EXAMPLECLASS_H
