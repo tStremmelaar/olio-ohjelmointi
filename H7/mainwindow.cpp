fedb064ed9e72b39961dcefca27fd41717dfede6
Thomas Stremmelaar <t.stremmie@gmail.com>
2024-12-12 14:57:16 +0200

    created project H7

---
+++ H7/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btnCount_clicked()
+{
+    ++counter;
+    QString s = QString::number(counter);
+}
